62f65561ea461ab97d95953609029569b9b6a869
zero885566 <zero888899@gmail.com>
2020-07-09 09:58:43 +0800

    first commit

---
+++ jamesphp/20200708.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo 'asd';
+    echo 'qwer';
+
+    echo __DIR__;
+    echo __FILE__;
+ 
+$a=1213;
+echo'a';
+
+
+
+ ?>
+</body>
+</html>
