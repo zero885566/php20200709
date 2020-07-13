1e5954c679daa073b4b26bfad174c94b86c3e302
zero885566 <zero888899@gmail.com>
2020-07-13 10:53:19 +0800

    20200713

---
+++ jamesphp/20200713.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // $ar = array(3,3,3,3);
+    // $ar1 = [4,4,4,4];
+    // print_r($ar);
+
+    // var_dump($ar1);
+
+    $p = array(
+        'a'=>'A',
+        'b'=>'B',
+        'c'=>'c',
+        666
+    );
+    print_r($p)
+
+
+
+
+    ?>
+
+
+</body>
+</html>
